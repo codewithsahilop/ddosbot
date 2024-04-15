b80d92259f98c28f691fded3a333ae7821cfb966
codewithsahilop <167054159+codewithsahilop@users.noreply.github.com>
2024-04-15 10:44:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,39 +7,39 @@ const token = '6959619997:AAGWIBICrM7iTwb9-YjcQmogwr_6ctVMVog';
 // Initialize bot with token
 const bot = new TelegramBot(token, { polling: true });
 
-// Fungsi untuk mencatat aktivitas penggunaan bot di console log
+// Function to record bot usage activities in the console log
 function logActivity(msg) {
   const user = msg.from;
   const chat = msg.chat;
   const command = msg.text.toLowerCase();
 
-  console.log(`Aktivitas Penggunaan Bot Telegram`);
+  console.log(`Telegram Bot Usage Activities`);
   console.log(`• User ID: ${user.id}`);
-  console.log(`• Username: ${user.username || 'Tidak ada'}`);
+  console.log(`• Username: ${user.username || 'There isn't any'}`);
   console.log(`• Chat ID: ${chat.id}`);
   console.log(`• Perintah: ${command}`);
 }
 
-// Event listener untuk pesan dari pengguna
+// Event listener for messages from users
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const command = msg.text.toLowerCase();
 
-  // Mencatat aktivitas penggunaan bot di console log
+  // Logs bot usage activity in the console log
   logActivity(msg);
 
-  // Menanggapi perintah /mix
+  // Respond to the /mix command
   if (command.startsWith('/mix')) {
-    // Mengekstrak argumen dari pesan
+    // Extracts arguments from the message
     const args = command.split(' ');
     const url = args[1];
     const time = args[2];
     const thread = args[3];
     const rate = args[4];
 
-    // Memeriksa apakah format pesan benar
+   // Checks whether the message format is correct
     if (args.length === 5 && url && time && thread && rate) {
-      // Menjalankan file mix.js dengan argumen yang diberikan
+      // Runs the mix.js file with the given arguments
       exec(`node mix.js ${url} ${time} ${thread} ${rate}`, (error, stdout, stderr) => {
         if (error) {
           console.error(`Error: ${error.message}`);
@@ -51,12 +51,12 @@ bot.on('message', (msg) => {
           bot.sendMessage(chatId, 'Successful');
           return;
         }
-        // Menampilkan output stdout jika berhasil
+        // Display stdout output if successful
         console.log(`stdout: ${stdout}`);
         bot.sendMessage(chatId, 'Proses telah dimulai.');
       });
     } else {
-      // Memberi tahu pengguna bahwa format pesan tidak benar
+      // Notifies the user that the message format is incorrect
       bot.sendMessage(chatId, 'Format pesan tidak benar. Gunakan format: /mix [url] [time] [thread] [rate]');
     }
   }
